e1430a359ff7214b8c278862a2766945f02a110c
AmazingWilson-hub <amazingwilson123@gmail.com>
2026-04-29 20:54:10 +0800

    Auto backup 2026-04-29 20:54 |  2 files changed, 2 insertions(+), 2 deletions(-)

--- tools/compare_video/renderer.py
+++ tools/compare_video/renderer.py
@@ -47,7 +47,7 @@ OCC3D_COLORS_BGR = np.array([
     [ 70, 206, 247],   # 15: manmade
     [152, 251, 152],   # 16: vegetation
     [255, 255, 255],   # 17: free_space (not rendered)
-    [255, 255, 255],   # 18: lane line (white)
+    [  0, 165, 255],   # 18: lane line (bright orange, BGR)
 ], dtype=np.uint8)
 
 # ---------------------------------------------------------------------------
--- tools/paper_figures/render_occ.py
+++ tools/paper_figures/render_occ.py
@@ -35,7 +35,7 @@ OCC3D_COLORS = np.array([
     [247, 206,  70],   # 15: manmade
     [152, 251, 152],   # 16: vegetation
     [ 50,  50,  50],   # 17: free  <- dark gray background
-    [255, 255, 255],   # 18: lane line (white)
+    [255, 165,   0],   # 18: lane line (bright orange, RGB)
 ], dtype=np.uint8)
 
 CLASS_NAMES = [
